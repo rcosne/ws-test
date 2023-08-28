24130cc3f70d9abfaa6a8fe09bcf1d3372933afc
Rémy Cosne <remy.cosne@sap.com>
2023-08-28 10:46:56 +0200

    Update security config

--- src/main/java/com/example/test/security/BasicAuthConfig.java
+++ src/main/java/com/example/test/security/BasicAuthConfig.java
@@ -5,9 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -26,11 +28,9 @@ public class BasicAuthConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
-        return http.build();
+        return http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults()).build();
     }
 }
